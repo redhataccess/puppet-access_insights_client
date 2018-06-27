e30eb0ca5a5b207609216970745ab04a61c5b19f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-06-27 17:01:13 +0200

    Refactor tesing infra

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
-describe 'access_insights_client' do
 
+describe 'access_insights_client' do
   context 'with defaults for all parameters' do
-    it { should contain_class('access_insights_client') }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_package('redhat-access-insights') }
+    it { is_expected.to contain_file('/etc/cron.daily/redhat-access-insights').with_ensure('link') }
+    it { is_expected.to contain_file('/etc/cron.weekly/redhat-access-insights').with_ensure('absent') }
   end
 end
